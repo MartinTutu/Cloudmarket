4da955b4393c406e464022a63acec49345218ef5
Martin <853756206@qq.com>
2016-02-11 17:59:18 +0800

    更新了我的fragment

--- src/com/martin/cloudmarket/MainActivity.java
+++ src/com/martin/cloudmarket/MainActivity.java
@@ -1,34 +1,111 @@
 package com.martin.cloudmarket;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
+	
+	private RadioGroup rg = null;
+//	private RadioButton rb1 = null;
+//	private RadioButton rb2 = null;
+//	private RadioButton rb3 = null;
+//	private RadioButton rb4 = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//通过控件的ID来得到代表控件的对象 
+		rg = (RadioGroup) findViewById(R.id.rg);
+//		rb1 = (RadioButton) findViewById(R.id.radio_button1);
+//		rb2 = (RadioButton) findViewById(R.id.radio_button2);
+//		rb3 = (RadioButton) findViewById(R.id.radio_button3);
+//		rb4 = (RadioButton) findViewById(R.id.radio_button4);		
+		//为RadioGroup设置监听器，需要注意的是，这里的监听器和Button控件的监听器有所不同
+		rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+		
+				switch (checkedId) {
+					case R.id.radio_button1:
+						//显示主SuperMarket的Fragment
+						showFragmentSuperMarket();
+						break;
+					case R.id.radio_button2:
+						//显示Order的Fragment
+						showFragmentOrder();
+						break;
+					case R.id.radio_button3:
+						//显示Shop的Fragment
+						showFragmentShop();
+						break;
+					case R.id.radio_button4:
+						//显示Mine的Fragment
+						showFragmentMine();
+						break;					
+					default:
+						break;
+				}
+			}
+			
+		});
+		//显示主SuperMarket的Fragment
+		showFragmentSuperMarket();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    
+    private void showFragmentSuperMarket(){
+    	//1.创建fragment对象
+    	FragmentSupermarket fs = new FragmentSupermarket();
+    	//2.获取fragment管理器
+    	FragmentManager fm = getFragmentManager();
+    	//3.开启事物
+    	FragmentTransaction ft = fm.beginTransaction();
+    	//4.显示fragment
+    	ft.replace(R.id.fl, fs);
+    	//5.提交
+    	ft.commit();
+    }
+    
+    private void showFragmentMine(){
+    	//1.创建fragment对象
+    	FragmentMine fmine = new FragmentMine();
+    	//2.获取fragment管理器
+    	FragmentManager fm = getFragmentManager();
+    	//3.开启事物
+    	FragmentTransaction ft = fm.beginTransaction();
+    	//4.显示fragment
+    	ft.replace(R.id.fl, fmine);
+    	//5.提交
+    	ft.commit();
+    }
+    
+    private void showFragmentOrder(){
+    	//1.创建fragment对象
+    	FragmentOrder fo = new FragmentOrder();
+    	//2.获取fragment管理器
+    	FragmentManager fm = getFragmentManager();
+    	//3.开启事物
+    	FragmentTransaction ft = fm.beginTransaction();
+    	//4.显示fragment
+    	ft.replace(R.id.fl, fo);
+    	//5.提交
+    	ft.commit();
+    }
+    
+    private void showFragmentShop(){
+    	//1.创建fragment对象
+    	FragmentShop fs = new FragmentShop();
+    	//2.获取fragment管理器
+    	FragmentManager fm = getFragmentManager();
+    	//3.开启事物
+    	FragmentTransaction ft = fm.beginTransaction();
+    	//4.显示fragment
+    	ft.replace(R.id.fl, fs);
+    	//5.提交
+    	ft.commit();
+    }
 }
